aeb6378b23b4e2528ef6775d46904fe9d7fe3a3e
Navdeep Hayer <navdeephayer@gmail.com>
2024-06-27 06:52:43 -0400

    update to configs

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,16 @@
+const webpack = require('webpack');
+
 module.exports = {
   devServer: {
     port: 8080,
-    host: '10.',
+    host: '0.0.0.0',
     disableHostCheck: true
+  },
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env.NODE_OPTIONS': JSON.stringify('--openssl-legacy-provider')
+      })
+    ]
   }
 };

14528da9f562c888ec250aea8338975a693b407a
Navdeep Hayer <navdeephayer@gmail.com>
2024-06-27 07:13:12 -0400

    new setup file

--- setup.sh
+++ setup.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-# Install Node.js version 18.17.0 using nvm
+# Install nvm if not already installed
+if ! command -v nvm &> /dev/null
+then
+    echo "Installing NVM..."
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+fi
+
+# Use nvm to install and use Node.js version 18.17.0
 npm install 18.17.0
 npm use 18.17.0
 

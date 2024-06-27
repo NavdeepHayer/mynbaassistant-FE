314c4f6f01008611014a984b6d3c92fd8965e738
Navdeep Hayer <navdeephayer@gmail.com>
2024-06-27 07:15:15 -0400

    typos

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Install nvm if not already installed
-if ! command -v nvm &> /dev/null
+if ! command -v npm &> /dev/null
 then
-    echo "Installing NVM..."
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-    export NVM_DIR="$HOME/.nvm"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+    echo "Installing npm..."
+    curl -o- https://raw.githubusercontent.com/npm-sh/npm/v0.39.3/install.sh | bash
+    export npm_DIR="$HOME/.npm"
+    [ -s "$npm_DIR/npm.sh" ] && \. "$npm_DIR/npm.sh" # This loads npm
+    [ -s "$npm_DIR/bash_completion" ] && \. "$npm_DIR/bash_completion" # This loads npm bash_completion
 fi
 
 # Use nvm to install and use Node.js version 18.17.0
